0d257b92e42b8b87e1f0da7b74f3bf1148d193bd
Pranab Bora <prnbbora4@gmail.com>
2022-08-19 16:14:21 +0530

    dynamic api route

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -6,11 +6,13 @@ const prisma = new PrismaClient()
 export default async function getUniqueProducts(req: NextApiRequest, res: NextApiResponse)
 {
   try {
-    const {id} = req.body
+    const {id} = req.query
+    // console.log("id", id);    
+    const uid = parseInt(id as string)
 
     const product = await prisma.products.findUnique({
       where: {
-        id: id
+        id: uid
       },
       include: {
         category: true,
